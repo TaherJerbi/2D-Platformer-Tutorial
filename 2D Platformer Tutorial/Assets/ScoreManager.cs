4bc180c037b5f46c64388df01008daed6b8a3751
Taher Jerbi <jerbi.taherr@gmail.com>
2020-02-26 18:48:05 +0100

    coins

---
+++ 2D Platformer Tutorial/Assets/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Coin")
+        {
+            score++;
+            scoreText.text = score.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
+}
