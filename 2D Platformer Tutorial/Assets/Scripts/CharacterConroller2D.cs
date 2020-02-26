6ed28c7559d9535de6d8d544e1b02f4868ddb719
Taher Jerbi <jerbi.taherr@gmail.com>
2020-02-26 18:21:52 +0100

    first commit

---
+++ 2D Platformer Tutorial/Assets/Scripts/CharacterConroller2D.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterConroller2D : MonoBehaviour
+{
+    public float speed;
+    public float smooth;
+    public float jumpForce;
+    bool facingRight = true;
+
+    bool grounded;
+
+    public Collider2D groundCheck;
+    public LayerMask groundLayer;
+
+    Animator animator;
+    Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //Get a reference to the Animator
+        animator = GetComponent<Animator>();
+        
+    }
+    private void Update()
+    {
+        //Use a grounded variable
+        grounded = groundCheck.IsTouchingLayers(groundLayer);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Get Input
+        float x = Input.GetAxisRaw("Horizontal");
+
+        //Update the animator
+        animator.SetBool("Running", x != 0);
+        animator.SetBool("Jumping", !grounded);
+        //if the input is moving the player right and the player is facing left...
+        if (x > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (x < 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+
+        //Update the player's velocity
+        Vector2 targetVelocity = new Vector2(x * speed, rb2d.velocity.y);
+
+        rb2d.velocity = Vector2.SmoothDamp(rb2d.velocity, targetVelocity,
+            ref targetVelocity, Time.deltaTime * smooth);
+
+        
+         
+
+}
+
+    void Flip()
+    {
+        //Invert the facingRight variable
+        facingRight = !facingRight;
+
+        //Flip the Character
+        Vector2 scale = transform.localScale;
+
+        scale.x *= -1;
+
+        transform.localScale = scale;
+    }
+}
---
+++ 2D Platformer Tutorial/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthManager : MonoBehaviour
+{
+    const float MAXHEALTH = 100f;
+    float health;
+    public Slider healthSlider;
+    private void Start()
+    {
+        health = MAXHEALTH;
+    }
+
+    void Die()
+    {
+        GetComponent<CharacterConroller2D>().enabled = false;
+        GetComponent<Animator>().SetBool("Dead", true);
+    }
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+        healthSlider.value = health / MAXHEALTH;
+        GetComponent<AudioSource>().Play();
+    }
+}
